f230eff31b905aec09f5dc9bbe1e37f9f4a159eb
vtt01uk <vtt01uk@gmail.com>
2016-02-25 12:47:36 +0000

    Show reviews stored in db onto restaurant page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,6 +10,8 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/1
   # GET /restaurants/1.json
   def show
+      #only see the views relateed to the restaurant id
+      @reviews = Review.where(restaurant_id: @restaurant.id)
   end
 
   # GET /restaurants/new
